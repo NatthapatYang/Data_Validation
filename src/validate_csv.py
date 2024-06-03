ace9ef2f20f7702d5991a06ce62bee2a9253c967
NatthapatYang <natthapatyang@gmail.com>
2024-06-03 14:01:05 +0700

    Delete unused variable and improve variable in loop

--- src/validate_csv.py
+++ src/validate_csv.py
@@ -8,18 +8,16 @@ import numpy as np
 df = pd.read_csv("demo_purchase.csv", sep=",", header=None, skiprows=[0] , on_bad_lines='skip')
 df.index = np.arange(1, len(df) + 1)
 total_row = len(df)
-current_line = 0
 total_valid = 0
 total_invalid = 0
 list_invalid_dateformat = []
 list_valid_dateformat = []
 for index, row in df.iterrows():
-    current_line += 1
     if date_month_year_format(row[2]):
-        list_valid_dateformat.append(current_line)
+        list_valid_dateformat.append(index)
         total_valid += 1
     else :
-        list_invalid_dateformat.append(current_line)
+        list_invalid_dateformat.append(index)
         total_invalid += 1
 
 print("List of invalid dateformat line : {} ".format(list_invalid_dateformat))
