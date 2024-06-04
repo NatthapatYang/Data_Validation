97ad512f19dcdeada9b5a4642678495b081c20ea
NatthapatYang <natthapatyang@gmail.com>
2024-06-04 21:44:37 +0700

    Use List instead Numpy library , add date/month/year format test and change attribute name in loop for easier to understand

--- src/validate_csv.py
+++ src/validate_csv.py
@@ -3,20 +3,21 @@ sys.path.append('./src')
 
 from validate_date_format import date_month_year_format
 import pandas as pd
-import numpy as np
 
-df = pd.read_csv("demo_purchase.csv", sep=",", header=None, skiprows=[0] , on_bad_lines='skip')
-df.index = np.arange(1, len(df) + 1)
+df = pd.read_csv("demo_purchase.csv", sep=",", header=None, skiprows=[0] , on_bad_lines='skip',
+                 names=["Order Number","Customer Tel","Created At","Product SKU","Price","Quantity","Total"])
+df.index = list(range(1, len(df) + 1))
+
 total_valid = 0
 total_invalid = 0
 list_invalid_dateformat = []
 list_valid_dateformat = []
-for index, row in df.iterrows():
-    if date_month_year_format(row[2]):
-        list_valid_dateformat.append(index)
+for row, column in df.iterrows():
+    if date_month_year_format(column["Created At"]):
+        list_valid_dateformat.append(row)
         total_valid += 1
     else :
-        list_invalid_dateformat.append(index)
+        list_invalid_dateformat.append(row)
         total_invalid += 1
 
 print("List of invalid dateformat line : {} ".format(list_invalid_dateformat))
--- src/validate_txt.py
+++ src/validate_txt.py
@@ -3,21 +3,21 @@ sys.path.append('./src')
 
 from validate_date_format import iso8601_format
 import pandas as pd
-import numpy as np
 
 df = pd.read_csv("datelist.txt", sep=",",header=None ,on_bad_lines='skip',
                  names=["Order ID","Customer Name","Dates","Product ID","Quantity","Total Price"])
-df.index = np.arange(1, len(df) + 1)
+df.index = list(range(1, len(df) + 1))
+
 total_valid = 0
 total_invalid = 0
 list_invalid_dateformat = []
 list_valid_dateformat = []
-for index, row in df.iterrows():
-    if iso8601_format(row["Dates"]):
-        list_valid_dateformat.append(index)
+for row, column in df.iterrows():
+    if iso8601_format(column["Dates"]):
+        list_valid_dateformat.append(row)
         total_valid += 1
     else :
-        list_invalid_dateformat.append(index)
+        list_invalid_dateformat.append(row)
         total_invalid += 1
 
 print("List of invalid dateformat line : {} ".format(list_invalid_dateformat))
